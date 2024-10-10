65d792110e57a12a030d6eba04db2abb06cdd2a5
istekhar <istekhararkam25@gmail.com>
2024-10-10 11:37:52 +0530

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,14 @@ import bell_icon from "../assets/bell_icon.svg";
 import profile_img from "../assets/profile_img.png";
 import caret_icon from "../assets/caret_icon.svg";
 
-
 const Navbar = () => {
   return (
-    <div className="flex fixed border-4 border-white w-full justify-between px-5 py-4">
+    <div className="fixed flex justify-between w-full px-5 py-4 border-4 border-white">
       <div className="flex items-center">
-       <div className="ml-5">
-        <img src={logo} alt="logo" className="h-9" />
-        </div> 
-        <ul className="cursor-pointer lg:flex gap-6 ml-5 hidden">
+        <div className="ml-5">
+          <img src={logo} alt="logo" className="h-9" />
+        </div>
+        <ul className="hidden gap-6 ml-20 text-lg cursor-pointer lg:flex">
           <li>Home</li>
           <li>TV Shows</li>
           <li>Movies</li>
@@ -22,18 +21,18 @@ const Navbar = () => {
           <li>Browse By Languages</li>
         </ul>
       </div>
-      <div className="flex cursor-pointer gap-6 items-center hidden md:block">
+      <div className="hidden gap-6 text-lg cursor-pointer lg:flex lg:items-center lg:ml-80">
         <img src={search_icon} alt="search-icon" className="font-2xl" />
-        <p>children</p>
-        <img src={bell_icon} alt="bell-icon"/>
-        </div>
-        <div className="flex gap-2 block">
-          <img src={profile_img} alt="profile-icon" className="rounded-lg"/>
-          <img src={caret_icon} alt="caret_icon"/>
-          <div>
-         
-          
-        </div>
+        <p>Children</p>
+        <img src={bell_icon} alt="bell-icon" />
+      </div>
+      <div className="flex items-center gap-2 ">
+        <img
+          src={profile_img}
+          alt="profile-icon"
+          className="rounded-lg h-7 w-7"
+        />
+        <img src={caret_icon} alt="caret_icon" />
       </div>
     </div>
   );
