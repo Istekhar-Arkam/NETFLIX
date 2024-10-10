f64d9d2e769876e0e53f6ba4b01ea976cc576ed8
istekhar <istekhararkam25@gmail.com>
2024-10-10 21:09:23 +0530

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.png";
 import search_icon from "../assets/search_icon.svg";
 import bell_icon from "../assets/bell_icon.svg";
 import profile_img from "../assets/profile_img.png";
 import caret_icon from "../assets/caret_icon.svg";
+import Dropdown from "./dropdown";
 
 const Navbar = () => {
+const [isDropdownVisible,setDropdownVisible]=useState(false);
+
+const handleMouseEnter =()=>{
+  setDropdownVisible(true);
+}
+  const handleMouseLeave=()=>{
+    setDropdownVisible(false);
+  }
   return (
-    <div className="fixed flex justify-between w-full px-5 py-4 border-4 border-white">
+    <div className="fixed flex justify-between w-full px-5 py-4">
       <div className="flex items-center">
-        <div className="ml-5">
+        <div className="">
           <img src={logo} alt="logo" className="h-9" />
         </div>
         <ul className="hidden gap-6 ml-20 text-lg cursor-pointer lg:flex">
@@ -26,13 +35,16 @@ const Navbar = () => {
         <p>Children</p>
         <img src={bell_icon} alt="bell-icon" />
       </div>
-      <div className="flex items-center gap-2 ">
+      <div className="relative flex items-center gap-2 cursor-pointer">
         <img
           src={profile_img}
           alt="profile-icon"
           className="rounded-lg h-7 w-7"
+          
         />
-        <img src={caret_icon} alt="caret_icon" />
+        <img src={caret_icon} alt="caret_icon" onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}/>
+        {isDropdownVisible && <Dropdown/>}
       </div>
     </div>
   );
