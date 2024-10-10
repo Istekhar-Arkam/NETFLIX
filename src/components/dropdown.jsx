b7039311418c5f02d12a41762d19ebe386552b22
istekhar <istekhararkam25@gmail.com>
2024-10-10 21:11:13 +0530

    code format

--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Dropdown = () => {
   return (
     <div className="absolute right-0 z-0 w-40 p-3 underline rounded cursor-pointer bg-zinc-900 top-14">
-    <p>Sign Out</p>
-  </div>
-  )
-}
+      <p>Sign Out</p>
+    </div>
+  );
+};
 
-export default Dropdown
+export default Dropdown;
