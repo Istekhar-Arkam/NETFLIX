665a169f25a883cf7b7c23b2c044ebb897cbd0a3
istekhar <istekhararkam25@gmail.com>
2024-10-11 20:21:49 +0530

    add hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-
-
+import React from "react";
+import Navbar from "../components/Navbar";
+import hero_banner from "../assets/hero_banner.jpg";
+import hero_title from "../assets/hero_title.png";
 const Home = () => {
   return (
-  <>
- <Navbar/>
-  </>
-   
-  )
-}
+    <div>
+      <Navbar />
+      <div className="">
+        <img src={hero_banner} alt="hero-banner" className="w-full " />
+        <div>
+          <img src={hero_title} alt="caption image" className="" />
+          <p>
+            Discovering his tires to a secret ancient order, young man living in
+            modern Istanbul embarks on quest to save the city from an immortal
+            enemy
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
