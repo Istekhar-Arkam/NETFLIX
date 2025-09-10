96b5cbad063290b212e5a80a14955a5db6333bc7
istekhar <istekhararkam25@gmail.com>
2025-09-11 01:59:52 +0530

    feat: add axios dependency and implement fetch for movie changes in TitleCard component

--- src/components/TitleCard.jsx
+++ src/components/TitleCard.jsx
@@ -1,6 +1,38 @@
+//import { useState } from "react";
 import cards_data from "../assets/cards/Cards_data";
+//import axios from "axios";
 
 const TitleCard = ({ title, category }) => {
+  // const [apiData,setApiData]=useState([])
+  // const options = {
+  //   method: "GET",
+  //   url: "https://api.freeapi.app/api/v1/public/youtube/videos",
+  //   params: {
+  //     page: "1",
+  //     limit: "10",
+  //     query: "javascript",
+  //     sortBy:
+  //       "keep%20one%3A%20mostLiked%20%7C%20mostViewed%20%7C%20latest%20%7C%20oldest",
+  //   },
+  //   headers: { accept: "application/json" },
+  // };
+  // (async () => {
+  //   try {
+  //     const { data } = await axios.request(options);
+  //     console.log(data);
+  //     setApiData(data)
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // })();
+
+  const url = 'https://api.themoviedb.org/3/movie/changes?page=1';
+const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error(err));
   return (
     <div className="mt-4 sm:w-full">
       <h2 className="px-2 ml-1 text-xl font-bold capitalize sm:ml-20 sm:my-1">
@@ -16,7 +48,7 @@ const TitleCard = ({ title, category }) => {
                 // card size
                 className="px-3 pb-4 cursor-pointer w-80"
               />
-              <p className="absolute uppercase bottom-5 right-6">{card.name}</p>
+              <p className="absolute uppercase bottom-5 right-6">{card.title}</p>
             </div>
           );
         })}
